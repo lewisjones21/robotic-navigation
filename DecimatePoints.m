de73ded11426e216b9b31f29f568d6c3a09cc87e
Lewis <laj30@cam.ac.uk>
2016-11-08 15:29:41 +0000

    Added dataset decimation function

---
+++ DecimatePoints.m
@@ -0,0 +1,10 @@
+function [points] = DecimatePoints(points, factor)
+
+N = floor(size(points,2) * factor);
+
+for i=1:N
+    randomVal = randi(size(points,2) - 1) + 1;
+    points(:, randomVal) = [];
+end
+
+end
--- GenerateMock3DData.m
+++ GenerateMock3DData.m
@@ -1,5 +1,9 @@
-function [points] = GenerateMock3DData(image)
+function [points] = GenerateMock3DData()
 
-
+points = [];
+points = [points, GenerateQuadrilateralPoints([-2, -2, 0], [4, 0, 0], [0, 0, 2], 8, 4)];
+points = [points, GenerateQuadrilateralPoints([-2, 2, 0], [4, 0, 0], [0, 0, 2], 8, 4)];
+points = [points, GenerateQuadrilateralPoints([-2, -2, 0], [0, 4, 0], [0, 0, 2], 8, 4)];
+points = [points, GenerateQuadrilateralPoints([2, -2, 0], [0, 4, 0], [0, 0, 2], 8, 4)];
 
 end
