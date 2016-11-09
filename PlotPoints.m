1852e9127ea5383618ff2ae63ba11b930782b47d
Lewis <laj30@cam.ac.uk>
2016-11-09 23:40:03 +0000

    Improved plotting functions

--- PlotPoints.m
+++ PlotPoints.m
@@ -3,6 +3,10 @@ function [] = PlotPoints(points)
 figure(1);
 
 scatter3(points(1,:), points(2,:), points(3,:))
+
+a = [1:size(points, 2)]'; b = num2str(a); c = cellstr(b);
+text(points(1,:), points(2,:), points(3,:), c);
+
 camproj('perspective')
 
 end
--- PlotPolygons.m
+++ PlotPolygons.m
@@ -1,12 +1,16 @@
 function [] = PlotPolygons(polygons)
 
 figure(1);
+colormap hsv;
+
+C = [0 0.25 0.5 0.75];
 
 for p = 1:size(polygons)
 
-    fill3(polygons(p).Points(1,:), polygons(p).Points(2,:), polygons(p).Points(3,:), [1 0 0])
+    fill3(polygons(p).Points(1,:), polygons(p).Points(2,:), polygons(p).Points(3,:), C)
     
 end
+
 camproj('perspective')
 
 end
