76418985b4fee5fd64ab8e845dd61c5ffdac68c3
Lewis <laj30@cam.ac.uk>
2016-11-09 23:40:57 +0000

    Added fix to ensure generating unique points

--- GenerateMock3DData.m
+++ GenerateMock3DData.m
@@ -1,9 +1,13 @@
 function [points] = GenerateMock3DData()
 
+proximityTolerance = 0.001;
+
 points = [];
 points = [points, GenerateQuadrilateralPoints([-2, -2, 0], [4, 0, 0], [0, 0, 2], 8, 4)];
 points = [points, GenerateQuadrilateralPoints([-2, 2, 0], [4, 0, 0], [0, 0, 2], 8, 4)];
 points = [points, GenerateQuadrilateralPoints([-2, -2, 0], [0, 4, 0], [0, 0, 2], 8, 4)];
 points = [points, GenerateQuadrilateralPoints([2, -2, 0], [0, 4, 0], [0, 0, 2], 8, 4)];
+
+points = uniquetol(points', proximityTolerance, 'ByRows', true)';
 
 end
