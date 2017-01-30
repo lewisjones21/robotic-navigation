ece0c7f09ae1c160543500c0e6c09f8be5320b64
Lewis <laj30@cam.ac.uk>
2017-01-30 00:53:21 +0000

    Populated class

--- Polygon.m
+++ Polygon.m
@@ -4,9 +4,18 @@ classdef Polygon
     
     properties
         Points
+        Normal
+        Class = -1  %Whether the polygon is 
+        Valid = 0   %Whether the points are coplanar
     end
     
     methods
+        function valid = checkValid()
+            r = round([obj.Value],2);
+        end
+        function r = multiplyBy(obj,n)
+            r = [obj.Value] * n;
+        end
     end
     
 end
