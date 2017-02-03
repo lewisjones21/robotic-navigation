9fc689e6a596f00923e8381c6f0fc45e951b6c45
Lewis <laj30@cam.ac.uk>
2017-02-03 02:49:31 +0000

    Created a function to plot shared edges

---
+++ PlotSharedEdges.m
@@ -0,0 +1,56 @@
+function [  ] = PlotSharedEdges( sharedEdges, triangles, points )
+%PLOTSHAREDEDGES Draws lines between triangles with shared edges
+%   Draws a plot similar to the Voronoi tessellation
+
+savedhold = ishold;
+
+figure(1);
+
+%As a test, first just highlight shared edges
+%XYZ = [ points(sharedEdges(:,3),:) points(sharedEdges(:,4),:) ];
+
+%Find the relevant triangle vertices
+firstTriangleIndices = sharedEdges(:,1);
+secondTriangleIndices = sharedEdges(:,2);
+firstTrianglesVertexIndices = triangles(firstTriangleIndices,:);
+secondTrianglesVertexIndices = triangles(secondTriangleIndices,:);
+firstTriangleCentres = (points(firstTrianglesVertexIndices(:,1),:) ...
+                        + points(firstTrianglesVertexIndices(:,2),:) ...
+                        + points(firstTrianglesVertexIndices(:,3),:)) / 3;
+secondTriangleCentres = (points(secondTrianglesVertexIndices(:,1),:) ...
+                        + points(secondTrianglesVertexIndices(:,2),:) ...
+                        + points(secondTrianglesVertexIndices(:,3),:)) / 3;
+
+X = [ firstTriangleCentres(:,1) secondTriangleCentres(:,1) ];
+Y = [ firstTriangleCentres(:,2) secondTriangleCentres(:,2) ];
+Z = [ firstTriangleCentres(:,3) secondTriangleCentres(:,3) ];
+
+% leftPointIndices = sharedEdges(:,3);
+% rightPointIndices = sharedEdges(:,4);
+% leftPointCoordinates = points(leftPointIndices,:);
+% rightPointCoordinates = points(rightPointIndices,:);
+
+%Draw lines across shared edges
+line(X(1,:), Y(1,:), Z(1,:), 'Color', 'magenta','LineWidth',1.5);
+hold on;
+for l = 2:size(X,1)
+    line(X(l,:), Y(l,:), Z(l,:), 'Color', 'magenta','LineWidth',1.5);
+end
+%line(XYZ(:, [1 4]), XYZ(:, [2 5]), XYZ(:, [3 6]));
+
+%axis(span);
+grid on;
+xlabel('x');
+ylabel('y');
+zlabel('z');
+camproj('perspective')
+
+%Reset the hold state to what it was before starting this function
+if savedhold
+    hold on;
+else
+    hold off;
+end
+
+end
+
