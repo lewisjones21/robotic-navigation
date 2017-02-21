a80d69febd9dbf73cee426378b8a093a8858fb07
Lewis <laj30@cam.ac.uk>
2017-02-21 16:25:08 +0000

    Added 'Basic' debug test option

--- RunDemo.m
+++ RunDemo.m
@@ -1,23 +1,56 @@
 
 hold off;
 
-Points = GenerateMock3DData();
-Points = AddNoise(Points, 0.003);
+if ~Basic
+    %Generate a point cloud
+    Points = GenerateMock3DData();
+    Points = AddNoise(Points, 0.003);
 
-Triangles = MyRobustCrust(Points);
-Triangles = CullTriangles(Triangles, Points, 0.7);
+    %Generate a closed mesh based on the point cloud
+    Triangles = MyRobustCrust(Points);
+    %Remove large triangles, which are typically capping a concave mesh
+    Triangles = CullTriangles(Triangles, Points, 0.7);
+    
+    %Decimate the mesh to simplify the data
+    [Triangles, Points] = reducepatch(Triangles, Points, 200 - Basic * 120);
+else
+    Points = [
+        0 0 0;
+        1 0 0;
+        1 1 0;
+        0 1 0;
+        0 2 0.5;
+        1 2 0.2;
+        2 0 0;
+        2 1 0;
+        ];
+    Triangles = [
+        1 2 3;
+        1 3 4;
+        3 4 6;
+        4 5 6;
+        2 7 8;
+        ];
+end
 
-ClassifiedTriangles = ClassifyPolygons(Triangles,Points,8,30);
+%Classify the triangles and sub-group them
+ClassifiedTriangles = ClassifyPolygons(Triangles, Points, 8, 30);
 GroundTriangles = ClassifiedTriangles(ClassifiedTriangles(:,4)==1,1:3);
 TraversableTriangles = [ GroundTriangles; ClassifiedTriangles(ClassifiedTriangles(:,4)==2,1:3) ];
 
+%Plot the mesh
 PlotTriangles(ClassifiedTriangles, Points);
 hold on;
 
+%Find sides that are common to more than one triangle
 SharedSides = FindSharedSides(TraversableTriangles, Points);
+%Draw links between triangles with shared sides
+%PlotSharedSides(SharedSides, TraversableTriangles, Points);
 
-PlotSharedSides(SharedSides, TraversableTriangles, Points);
-
-Waypoints = PlaceWaypoints(TraversableTriangles, Points);
-
+%Place waypoints onto the mesh
+%Waypoints = PlaceWaypoints(TraversableTriangles, Points, SharedSides);
+[Waypoints, Edges] = GenerateNavigationGraph(TraversableTriangles, Points, SharedSides);
+%Plot the edges
+PlotEdges(Edges, Waypoints);
+%Plot the waypoints
 PlotWaypoints(Waypoints);
