4ff54f85e0d652aeb03175a4c618bd136134f83f
Lewis <laj30@cam.ac.uk>
2017-05-12 23:42:42 +0100

    Created basic initialisation script

---
+++ Initialise.m
@@ -0,0 +1,15 @@
+function [  ] = Initialise(  )
+%INITIALISE Add relevant filepaths
+%   Adds relevant filepaths to the working directory
+
+
+addpath Analysis
+addpath Drawing
+addpath Mapping
+addpath Mapping/crust_algorithm
+addpath Pathfinding
+addpath Simulation
+
+
+end
+
