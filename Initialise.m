85239300b8a14d75da28ad0e2a0496ba0e882025
Lewis <laj30@cam.ac.uk>
2017-05-18 14:35:35 +0100

    Imported interface for Kinect 2

--- Initialise.m
+++ Initialise.m
@@ -1,14 +1,16 @@
-function [  ] = Initialise(  )
+function [] = Initialise()
 %INITIALISE Add relevant filepaths
 %   Adds relevant filepaths to the working directory
 
 
-addpath Analysis
-addpath Drawing
-addpath Mapping
-addpath Mapping/crust_algorithm
-addpath Pathfinding
-addpath Simulation
+addpath('Analysis');
+addpath('Drawing');
+addpath('Mapping');
+addpath('Mapping/Kinect 2 Interface/jrterven-Kin2-ff46b0c');
+addpath('Mapping/Kinect 2 Interface/jrterven-Kin2-ff46b0c/Mex');
+addpath('Mapping/Mesh Triangulation/crust_algorithm');
+addpath('Pathfinding');
+addpath('Simulation');
 
 
 end
