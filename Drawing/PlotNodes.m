8ef90e4181542e88b55140726d2e271618b272d4
Lewis <laj30@cam.ac.uk>
2017-05-26 14:40:26 +0100

    Made drawing indices optional

--- Drawing/PlotNodes.m
+++ Drawing/PlotNodes.m
@@ -1,6 +1,7 @@
-function [] = PlotNodes(nodes, colour)
+function [] = PlotNodes(nodes, colour, noIndices)
 %PLOTNODES Plots the nodes as large markers
-%   
+%   Draw indices by default, but allow opt-out by inputting a third
+%   parameter
 
 %Validate the inputs
 if size(nodes, 1) <= 0
@@ -23,10 +24,13 @@ figure(1);
 %Draw the path coords (highlighted)
 scatter3(nodes(:,1), nodes(:,2), nodes(:,3), 400, 'MarkerFaceColor', colour)
 
-%Draw indices showing the order of the path coords
-a = [1:size(nodes, 1)]'; b = num2str(a); c = cellstr(b);
-offset = ones(size(nodes, 1), 1) * 0.02;
-text(nodes(:,1) - offset, nodes(:,2), nodes(:,3) + offset, c, 'Color', 'white');
+if nargin < 3
+    %Draw indices showing the order of the path coords
+    a = [1:size(nodes, 1)]'; b = num2str(a); c = cellstr(b);
+    offset = ones(size(nodes, 1), 1) * 0.02;
+    text(nodes(:,1) - offset, nodes(:,2), nodes(:,3) + offset, c, ...
+        'Color', 'white');
+end
 
 %axis(span);
 grid on;
