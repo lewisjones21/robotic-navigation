04a36850216a93a078704bf98c224584a0b2d469
Lewis <laj30@cam.ac.uk>
2017-05-27 14:04:32 +0100

    Commit of all results so far

--- Initialise.m
+++ Initialise.m
@@ -6,11 +6,13 @@ function [] = Initialise()
 addpath('Analysis');
 addpath('Analysis/Testing');
 addpath('Drawing');
+addpath('Inputs');
 addpath('Mapping');
 addpath('Mapping/Kinect 2 Interface/jrterven-Kin2-ff46b0c');
 addpath('Mapping/Kinect 2 Interface/jrterven-Kin2-ff46b0c/Mex');
 addpath('Mapping/Mesh Triangulation/crust_algorithm');
 addpath('Pathfinding');
+addpath('Results');
 addpath('Simulation');
 
 
