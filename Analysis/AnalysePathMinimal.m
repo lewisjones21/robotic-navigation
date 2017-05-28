6adfa3333eb7e691c0550e844511c9e78cd7f221
Lewis <laj30@cam.ac.uk>
2017-05-28 18:15:43 +0100

    Added projected path length metric

--- Analysis/AnalysePathMinimal.m
+++ Analysis/AnalysePathMinimal.m
@@ -1,4 +1,5 @@
-function [ pathLength, directDistance, factorAboveDirect ] ...
+function [ pathLength, directDistance, factorAboveDirect, ...
+    projectedFactorAboveDirect ] ...
         = AnalysePathMinimal( pathWaypointIndices, ...
             waypoints, triangles, points, pathCoords, coordErrors )
 %ANALYSEPATHMINIMAL Returns length data about the path
@@ -8,6 +9,7 @@ function [ pathLength, directDistance, factorAboveDirect ] ...
 pathLength = [];
 directDistance = [];
 factorAboveDirect = [];
+projectedFactorAboveDirect = [];
 
 %Validate the path coord inputs
 if size(pathCoords, 1) <= 0
@@ -59,12 +61,20 @@ pathWaypoints = waypoints(pathWaypointIndices,:);
 %(length of the path through all path waypoints); the length of the path
 %that was found
 pathLength = FindPathLength(pathWaypoints);
+%Find the length of the path if it were projected onto the z=0 plane (as in
+%the Plane environment)
+projectedPathLength = FindPathLength( ...
+    [ pathWaypoints(:, 1:2), zeros(size(pathWaypoints, 1), 1) ]);
 
 %Correct the path length to make it more representative
 %   + the distance required at either end to reach the target coords
 %	+ the distance required to reach the intermediate target coords and
 %       then get back to the path to continue traversing it
 pathLength = pathLength ...
+    + sum(coordErrors([1, size(coordErrors, 1)])) ...
+    + 2 * sum(coordErrors(2:size(coordErrors,1)-1));
+%Also do this for the projected length
+projectedPathLength = projectedPathLength ...
     + sum(coordErrors([1, size(coordErrors, 1)])) ...
     + 2 * sum(coordErrors(2:size(coordErrors,1)-1));
 
@@ -75,6 +85,8 @@ factorAboveDirect = (pathLength - directDistance) / directDistance;
 %waypoints don't quite reach the requested coords, so actually take a
 %shorter route); -1 indicates zero path length (no path found)
 
+%Find the equivalent value for the projected path length
+projectedFactorAboveDirect = (projectedPathLength - directDistance) / directDistance;
 
 end
 
